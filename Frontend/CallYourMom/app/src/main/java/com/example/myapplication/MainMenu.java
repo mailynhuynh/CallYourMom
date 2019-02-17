6c1b80da6e9423e6fe2f1e0e10020ab1a4ac58f6
John <jhwagner@iastate.edu>
2019-02-17 13:41:31 -0600

    Added MainMenu java file and xml

---
+++ Frontend/CallYourMom/app/src/main/java/com/example/myapplication/MainMenu.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainMenu extends AppCompatActivity
+    Button b1, b2;
+
+    @Override
+    protectected void onCreate(Bundle savedInstanceState){
+
+            }
+
+
+
+    }
+}

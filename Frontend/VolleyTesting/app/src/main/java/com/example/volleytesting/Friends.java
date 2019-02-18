900c6bd6e1c9e887124c2b0e0815778fba910626
Zoe Sanders <ZoeS@zsanders.student.iastate.edu>
2019-02-17 18:09:34 -0600

    Made another page to receive JSON data from server.

---
+++ Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Friends.java
@@ -0,0 +1,100 @@
+package com.example.volleytesting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import app.AppController;
+
+public class Friends extends AppCompatActivity {
+
+    Button back;
+    Button getFriends;
+    TextView friends;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        //Initialize friends text field
+        friends = findViewById(R.id.friends);
+        //Back button
+        back = findViewById(R.id.backb);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backScreen = new Intent(getApplicationContext(), Login.class);
+                startActivity(backScreen);
+            }
+        });
+
+        getFriends = findViewById(R.id.getFriends);
+        getFriends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseJSONFriends();
+            }
+        });
+
+
+    }
+
+
+    private void parseJSONFriends()
+    {
+        String url = "http://cs309-bs-3.misc.iastate.edu:8080/users";
+
+        RequestQueue rq = AppController.getInstance().getRequestQueue();
+        JsonArrayRequest arrayRequest = new JsonArrayRequest(Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for(int i = 0; i < response.length(); i++)
+                {
+                    try {
+                        JSONObject obj = response.getJSONObject(i);
+
+                        String fName = obj.getString("firstName");
+                        String lName = obj.getString("lastName");
+                        int phoneNum = obj.getInt("telephone");
+
+                        friends.append(fName+" "+lName+": Phone Number: "+phoneNum+"\n");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+
+                }
+
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                error.printStackTrace();
+
+            }
+        });
+        rq.add(arrayRequest);
+    }
+}
--- Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Login.java
+++ Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Login.java
@@ -1,5 +1,6 @@
 package com.example.volleytesting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,30 +9,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     /**
      * This was taken from
      * https://www.mytrendin.com/send-data-to-remote-server-in-android-using-volley-library/
      */
-    EditText name, email, password;
-    Button save;
+    EditText name, email, password, lastName, phoneNumber, userName;
+    Button save, displayFriends;
     String url = "http://cs309-bs-3.misc.iastate.edu:8080/users";
     RequestQueue queue;
     JsonObjectRequest request;
@@ -40,39 +36,60 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Edit Field initialization
         name = (EditText) findViewById(R.id.name);
         email = (EditText) findViewById(R.id.email);
         password = (EditText) findViewById(R.id.password);
+        lastName = findViewById(R.id.lastName);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        userName = findViewById(R.id.username);
+        // Button initialization
         save = (Button) findViewById(R.id.save);
+        displayFriends = findViewById(R.id.friends);
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 final String username = name.getText().toString();
                 final String useremail = email.getText().toString();
                 final String userpassword = password.getText().toString();
+                final String lName = lastName.getText().toString();
+                final String phoneNum = phoneNumber.getText().toString();
+                final String userN = userName.getText().toString();
 
-                SendUser(username, useremail, userpassword);
+                SendUser(username, lName, useremail, phoneNum, userN, userpassword);
 
+
+            }
+        });
+
+        displayFriends.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent nextScreen = new Intent(getApplicationContext(), Friends.class);
+                startActivity(nextScreen);
 
             }
         });
     }
 
-    private void SendUser(String name, String email, String password)
+    private void SendUser(String name, String lastName, String email, String
+            phoneNum, String userName, String password)
     {
         //Map<String, String> params = new HashMap<>();
         JSONObject params = new JSONObject();
         try {
             params.put("firstName", name);
-            params.put("lastName", password);
-            params.put("userName", email);
-            params.put("password", 13322);
-            params.put("address", "uwbviugbw");
-            params.put("telephone", "5155050743");
+            params.put("lastName", lastName);
+            params.put("userName", userName);
+            params.put("password", password);
+            params.put("address", email);
+            params.put("telephone", phoneNum);
         }
         catch (JSONException e) { e.printStackTrace(); }
 
-        queue = Volley.newRequestQueue(MainActivity.this);
+        queue = Volley.newRequestQueue(Login.this);
         request = new JsonObjectRequest(Request.Method.POST, url, params, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {

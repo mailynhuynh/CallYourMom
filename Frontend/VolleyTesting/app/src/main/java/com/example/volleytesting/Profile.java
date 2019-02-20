8b730694acacd4c276ca32c72841e4026470ce9f
jhwagner <jhwagner@iastate.edu>
2019-02-19 21:19:50 -0600

    Designed Profile Page like screen sketch, and started coding buttons

--- Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Profile.java
+++ Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Profile.java
@@ -2,12 +2,32 @@ package com.example.volleytesting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Profile extends AppCompatActivity {
+    Button b1, b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+   b1 = (Button) findViewByID(R.id.Back);
+    b2 = (Button) findViewByID(R.id.Back);
+
+    b1.setOnClickLister(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+
+            )}
+    }
+
+    b2.setOnClickLister(new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+
+        }
     }
 }

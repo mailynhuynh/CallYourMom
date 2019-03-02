0e5df9fe307be20a01dfdad28ec41c139b3ee0f6
Zoe Sanders <ZoeS@zsanders.student.iastate.edu>
2019-03-02 13:43:42 -0600

    New commit

---
+++ Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/ReminderCreator.java
@@ -0,0 +1,13 @@
+package com.example.volleytesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReminderCreator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder_creator);
+    }
+}

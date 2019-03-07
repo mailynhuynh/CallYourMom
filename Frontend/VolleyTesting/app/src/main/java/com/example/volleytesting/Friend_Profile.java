d75fb4dcebd04d851d5ea773ccd44203d833ded6
John <jhwagner@iastate.edu>
2019-03-07 16:54:52 -0600

    changed profile to Friend Profile

--- Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Friend_Profile.java
+++ Frontend/VolleyTesting/app/src/main/java/com/example/volleytesting/Friend_Profile.java
@@ -5,13 +5,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class User_Profile extends AppCompatActivity {
+public class Friend_Profile extends AppCompatActivity {
     Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user_profile);
+        setContentView(R.layout.activity_friend_profile);
         b1 = (Button) findViewById(R.id.Back);
 
         b1.setOnClickListener(new View.OnClickListener(){

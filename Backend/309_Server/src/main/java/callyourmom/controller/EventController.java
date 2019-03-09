1f795db4ecd862705181639b8a4d651cb13e2464
Mailyn Huynh <mailynhuynh@gmail.com>
2019-03-08 21:58:28 -0600

    Added event REST classes

---
+++ Backend/309_Server/src/main/java/callyourmom/controller/EventController.java
@@ -0,0 +1,31 @@
+package callyourmom.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+
+import callyourmom.service.EventService;
+import callyourmom.user.Event;
+
+@RestController
+public class EventController {
+	@Autowired // @Autowired attribute injects UserService into this class
+	EventService eventService;
+	
+	@GetMapping("/events")
+	List<Event> get() {
+	    return eventService.getAll();
+	}
+	
+	@GetMapping("/events/{id}")
+	Event get(@PathVariable Long id) {
+	    return eventService.get(id);
+	}
+
+	@PostMapping("/events")
+	Event post(@RequestBody Event event) {
+	    return eventService.create(event);
+	}
+}
--- Backend/309_Server/src/main/java/callyourmom/user/Event.java
+++ Backend/309_Server/src/main/java/callyourmom/user/Event.java
@@ -25,7 +25,7 @@ public class Event {
     @NotFound(action = NotFoundAction.IGNORE)
     private Integer id;
 
-    @Column(name = "time")
+	@Column(name = "time")
     @NotFound(action = NotFoundAction.IGNORE)
     private String time;
 
@@ -48,70 +48,79 @@ public class Event {
     @Column(name = "tags")
     @NotFound(action = NotFoundAction.IGNORE)
     private String tags;
-
-
+    
+    @Column(name = "description")
+    @NotFound(action = NotFoundAction.IGNORE)
+    private String description;
+    
     public Integer getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public boolean isNew() {
-        return this.id == null;
-    }
+	public String getTime() {
+		return time;
+	}
 
-    public String getTime() {
-        return this.time;
-    }
+	public void setTime(String time) {
+		this.time = time;
+	}
 
-    public void setFirstName(String time) {
-        this.time = time;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getTitle() {
-        return this.title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public String getLocation() {
-        return this.location;
-    }
+	public void setLocation(String location) {
+		this.location = location;
+	}
 
-    public void setLocation(String location) {
-        this.location = location;
-    }
+	public String getFriends_invited() {
+		return friends_invited;
+	}
 
-    public String getFriends_Invited() {
-        return this.friends_invited;
-    }
+	public void setFriends_invited(String friends_invited) {
+		this.friends_invited = friends_invited;
+	}
 
-    public void setFriends_Intived(String friends_invited) {
-        this.friends_invited = friends_invited;
-    }
-    
-    public String getFriends_Going() {
-        return this.friends_going;
-    }
-    
-    public void setFriends_Going(String friends_going) {
-        this.friends_going = friends_going;
-    }
+	public String getFriends_going() {
+		return friends_going;
+	}
 
-    @Override
-    public String toString() {
-        return new ToStringCreator(this)
+	public void setFriends_going(String friends_going) {
+		this.friends_going = friends_going;
+	}
 
-                .append("id", this.getId())
-                .append("new", this.isNew())
-                .append("time", this.getTime())
-                .append("title", this.getTitle())
-                .append("location", this.location)
-                .append("friends_invited", this.friends_invited).toString();
-    }
-    
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Event [id=" + id + ", time=" + time + ", title=" + title + ", location=" + location
+				+ ", friends_invited=" + friends_invited + ", friends_going=" + friends_going + ", tags=" + tags
+				+ ", description=" + description + "]";
+	}
 }

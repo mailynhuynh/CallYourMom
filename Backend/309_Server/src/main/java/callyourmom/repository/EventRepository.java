e64b1ae90b6f584c45fc9fe6bddd13aab138dd52
Mailyn Huynh <mailynhuynh@gmail.com>
2019-03-10 12:01:10 -0500

    polished event services

--- Backend/309_Server/src/main/java/callyourmom/repository/EventRepository.java
+++ Backend/309_Server/src/main/java/callyourmom/repository/EventRepository.java
@@ -4,6 +4,6 @@ import callyourmom.user.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface EventRepository extends JpaRepository<Event, Long> {
+public interface EventRepository extends JpaRepository<Event, Integer> {
     Event findByTitle(String title);
-}
+}   

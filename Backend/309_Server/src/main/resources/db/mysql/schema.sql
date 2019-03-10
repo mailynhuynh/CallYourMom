0c12b25be2170bf33f027c7299ed6dd90bac7638
Ashley Hsu <ashleyhsu@huazhangs-mbp.student.iastate.edu>
2019-03-10 13:30:35 -0500

    edit mysql code

--- Backend/309_Server/src/main/resources/db/mysql/schema.sql
+++ Backend/309_Server/src/main/resources/db/mysql/schema.sql
@@ -19,7 +19,35 @@ CREATE TABLE IF NOT EXISTS CallYourMomEvents(
   title VARCHAR(30) NOT NULL,
   location VARCHAR(30) NOT NULL,
   friends_invited VARCHAR(100),
-  friends_going VARCHAR(100)
+  friends_going VARCHAR(100),
+  description VARCHAR(300)
 ) engine=InnoDB;
+
+
+
+CREATE TABLE IF NOT EXISTS CallYourMomReminders(
+id INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  time VARCHAR(30) NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  location VARCHAR(30) NOT NULL
+) engine=InnoDB;
+  
+  
+CREATE TABLE IF NOT EXISTS CallYourMomSettings(
+id INTEGER(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+theme boolean NOT NULL,
+notification boolean NOT NULL,
+sharing VARCHAR(30)
+
+)
+
+
+
+  
+
+
+
+
+
 
 

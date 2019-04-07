0add801c9e44d34f1ea2f73a1875de41889d751a
Ashley Hsu <ashleyhsu@huazhangs-mbp.student.iastate.edu>
2019-04-07 15:19:19 -0500

    added two mockito test classes for Event and User

---
+++ Backend/309_Server/src/main/java/mockito/MockitoTest_Event.java
@@ -0,0 +1,80 @@
+package mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import callyourmom.repository.*;
+import callyourmom.service.*;
+import callyourmom.user.*;
+
+public class MockitoTest_Event {
+
+	@InjectMocks
+	EventService service;
+
+	@Mock
+	EventRepository repo;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		Event r = new Event();
+		r.setId(1);
+
+	}
+
+	@Test
+	public void getEventByIDTest() {
+
+		Event r = new Event();
+		r.setId(1);
+		r.setTitle("MockitoTest1");
+
+		when(repo.getOne(1)).thenReturn(r);
+
+		Event result = service.get(1);
+		assertEquals("MockitoTest1", result.getTitle());
+	}
+
+	@Test
+	public void getAllEventTest() {
+
+		List<Event> rList = new ArrayList<>();
+		Event r1 = new Event();
+		Event r2 = new Event();
+		Event r3 = new Event();
+
+		rList.add(r1);
+		rList.add(r2);
+		rList.add(r3);
+
+		when(repo.findAll()).thenReturn(rList);
+		List<Event> result = service.getAll();
+		assertEquals(rList, result);
+	}
+
+	@Test
+	public void createEventTest() {
+
+		Event newEvent = new Event();
+		newEvent.setId(3);
+		newEvent.setTitle("MockitoTest3");
+
+		when(repo.save(newEvent)).thenReturn(newEvent);
+		Event result = service.create(newEvent);
+		String title = result.getTitle();
+		assertEquals("MockitoTest3", title);
+	}
+
+}
---
+++ Backend/309_Server/src/main/java/mockito/MockitoTest_User.java
@@ -0,0 +1,81 @@
+package mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import callyourmom.repository.*;
+import callyourmom.service.*;
+import callyourmom.user.*;
+
+public class MockitoTest_User {
+
+	@InjectMocks
+	UserService service;
+
+	@Mock
+	UserRepository repo;
+
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+		User r = new User();
+		r.setId(1);
+
+	}
+
+	@Test
+	public void getUserByIDTest() {
+
+		User r = new User();
+		r.setId(1);
+		r.setUserName("Zoe~");
+
+		when(repo.getOne(1)).thenReturn(r);
+
+		User result = service.get(1);
+		assertEquals("Zoe~", result.getUserName());
+	}
+
+	@Test
+	public void getAllUserTest() {
+
+		List<User> rList = new ArrayList<>();
+		User r1 = new User();
+		User r2 = new User();
+		User r3 = new User();
+
+		rList.add(r1);
+		rList.add(r2);
+		rList.add(r3);
+
+		when(repo.findAll()).thenReturn(rList);
+		List<User> result = service.getAll();
+		assertEquals(rList, result);
+	}
+
+	@Test
+	public void createUserTest() {
+
+		User newUser = new User();
+		newUser.setId(3);
+		newUser.setUserName("I'm a new user");
+
+		when(repo.save(newUser)).thenReturn(newUser);
+		User result = service.create(newUser);
+		String un = result.getUserName();
+		assertEquals("I'm a new user", un);
+	}
+
+}
+

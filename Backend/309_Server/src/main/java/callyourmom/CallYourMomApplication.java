23379e29ff7f54eef12e92ee89752920725b7340
Mailyn Huynh <mailynhuynh@gmail.com>
2019-04-28 12:11:23 -0500

    added CallYourMomApplication

---
+++ Backend/309_Server/src/main/java/callyourmom/CallYourMomApplication.java
@@ -0,0 +1,15 @@
+package callyourmom;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CallYourMomApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(CallYourMomApplication.class, args);
+	}
+
+}
